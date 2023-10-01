eced332f89be6dd347b981d0337a0e62813fa73f
stasbaz <bazuluk989@gmail.com>
2023-09-30 23:36:37 -0600

    Final version of project

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# Professional README Generator Starter Code
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+# README-generator
+
+## Description
+this is simple README generator throughyourr terminal
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+To install the necessary dependencies, run:
+```
+To install the necessary dependencies, run the following command:
+```
+
+## Usage
+npm install
+
+## License
+This project is licensed under the MIT licence license.
+
+## Contributing
+Fork the repo, create a new branch, make your changes and commit them, push ur branch, crate a pull request.
+
+## Tests
+To run tests, use:
+```
+run it in terminal
+```
+
+## Questions
+If you have any questions, please reach out to [bazuluk989@gmail.com](mailto:bazuluk989@gmail.com).
+        
---
+++ index.js
@@ -0,0 +1,103 @@
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a brief description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How should users install your application?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How should users use your application?'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What license is your project under?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute to your project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How can users run tests for your application?'
+    },
+];
+
+// TODO: Create a function to write the README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('README.md has been successfully generated.');
+        }
+    });
+}
+
+// TODO: Create a function to initialize the app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        // Generate the README content
+        const readmeContent = `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+To install the necessary dependencies, run:
+\`\`\`
+${answers.installation}
+\`\`\`
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+To run tests, use:
+\`\`\`
+${answers.tests}
+\`\`\`
+
+## Questions
+If you have any questions, please reach out to [bazuluk989@gmail.com](mailto:bazuluk989@gmail.com).
+        `;
+        // Write the README content to a file
+        writeToFile('README.md', readmeContent);
+    });
+}
+
+// Function call to initialize the app
+init();
